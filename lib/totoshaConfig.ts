e7926fe7610ee80e236794a86d875f2c13315dbd
nexora-kz <zhanat.serikov@gmail.com>
2026-06-20 22:22:25 +0500

    TOTOSHA v035: add shared site config

---
+++ lib/totoshaConfig.ts
@@ -0,0 +1,32 @@
+export const TOTOSHA_VERSION = 'v035';
+export const TOTOSHA_BUILD_DATE = '2026-06-20';
+export const TOTOSHA_DEPLOY_LABEL = 'NEXORA Deploy ✓';
+
+export const TOTOSHA_CONTACTS = {
+  phoneDigits: '77071230108',
+  phoneDisplay: '+7 707 123 01 08',
+  telUrl: 'tel:+77071230108',
+  whatsappUrl: 'https://wa.me/77071230108',
+  instagramUrl: 'https://www.instagram.com/totoshakids/',
+  telegramUrl: 'https://t.me/totoshakids',
+  mapUrl:
+    'https://2gis.kz/astana/search/%D0%A2%D0%BE%D1%82%D0%BE%D1%88%D0%B0%20%D0%90%D0%BB%D0%B8%D1%85%D0%B0%D0%BD%20%D0%91%D0%BE%D0%BA%D0%B5%D0%B9%D1%85%D0%B0%D0%BD%2029%D0%B0',
+} as const;
+
+export const HOME_GALLERY = [
+  {
+    src: '/totosha-media/2022/Новый_год_2022/001_photo_10@26-12-2022_14-37-49.jpg',
+    alt: 'Новый год в Тотоша',
+    title: 'Праздники',
+  },
+  {
+    src: '/totosha-media/2022/Новый_год_2022/002_photo_17@26-12-2022_14-37-49.jpg',
+    alt: 'Праздник в детском саду Тотоша',
+    title: 'Мероприятия',
+  },
+  {
+    src: '/totosha-media/2022/Новый_год_2022/003_photo_23@26-12-2022_14-37-49.jpg',
+    alt: 'Жизнь детского сада Тотоша',
+    title: 'Атмосфера',
+  },
+] as const;
